723985ccfbf028c3c49686b8571d11d4a359ffa9
Mark Wright (Koni) <mhw6@cornell.edu>
2015-07-08 22:10:41 -0400

    Revert "Cummulative changes for PVE modifications, as they were (no history avail.)"
    Want these changes to be on PVE branch instead
    
    This reverts commit aa5a542959b8ab3597eb3083cd520d2dff8cba07.

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
-export INTELROOT=/opt/intel
+export INTELROOT=/home/hmkang/intel/Compiler/11.1/069
 export MKLROOT=${INTELROOT}/mkl
 export MKLINCLUDE=${MKLROOT}/include
 
-. $INTELROOT/bin/iccvars.sh intel64
-. $MKLROOT/bin/mklvars.sh intel64
-export MKLLIBPATH=${MKLROOT}/lib/intel64
+sh $INTELROOT/bin/iccvars.sh intel64
+sh $MKLROOT/tools/environment/mklvars64.sh
+export MKLLIBPATH=${MKLROOT}/lib/em64t
 export INTELLIBPATH=${INTELROOT}/lib/intel64
 export BINPATH=${INTELROOT}/bin/intel64
 
-icc -Wall -wd1419 -O2 -I $MKLINCLUDE emmax-kin.c -Wl,--start-group -L $MKLLIBPATH -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -liomp5 -Wl,--end-group -lpthread -lz -o emmax-kin-intel64
+$BINPATH/icc -Wall -wd1419 -O2 -I $MKLINCLUDE emmax-kin.c -Wl,--start-group $MKLLIBPATH/libmkl_intel_lp64.a $MKLLIBPATH/libmkl_intel_thread.a $MKLLIBPATH/libmkl_core.a $INTELLIBPATH/libguide.a -Wl,--end-group -lpthread -lz -o emmax-kin-intel64
 
-icc -Wall -wd1419 -wd1418 -wd869 -wd920 -wd981 -wd1572 -O2 -I $MKLINCLUDE emmax.c -Wl,--start-group $MKLLIBPATH/libmkl_intel_lp64.a $MKLLIBPATH/libmkl_intel_thread.a $MKLLIBPATH/libmkl_core.a -liomp5 -Wl,--end-group -lpthread -lz -o emmax-intel64
+$BINPATH/icc -Wall -wd1419 -wd1418 -wd869 -wd920 -wd981 -wd1572 -O2 -I $MKLINCLUDE emmax.c -Wl,--start-group $MKLLIBPATH/libmkl_intel_lp64.a $MKLLIBPATH/libmkl_intel_thread.a $MKLLIBPATH/libmkl_core.a $INTELLIBPATH/libguide.a -Wl,--end-group -lpthread -lz -o emmax-intel64
 
-icc -Wall -wd1419 -wd1418 -wd869 -wd920 -wd981 -wd1572 -O2 -I $MKLINCLUDE emmax-predict.cc -Wl,--start-group $MKLLIBPATH/libmkl_intel_lp64.a $MKLLIBPATH/libmkl_intel_thread.a $MKLLIBPATH/libmkl_core.a -liomp5 -Wl,--end-group -lpthread -lz -o emmax-predict-intel64
+$BINPATH/icc -Wall -wd1419 -wd1418 -wd869 -wd920 -wd981 -wd1572 -O2 -I $MKLINCLUDE emmax-predict.cc -Wl,--start-group $MKLLIBPATH/libmkl_intel_lp64.a $MKLLIBPATH/libmkl_intel_thread.a $MKLLIBPATH/libmkl_core.a $INTELLIBPATH/libguide.a -Wl,--end-group -lpthread -lz -o emmax-predict-intel64
